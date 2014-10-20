b48ba4ddcd2b7e4a241c200f4f1810cfe087aad0
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2014-10-20 17:26:59 +0300

    Test tag in hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,7 +7,13 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import Request
 
-config = json.loads(open('./config/config.json'))
+# config = json.loads(open('./config/config.json', 'r').read())
+
+config = {
+    "repo_name": 'test'
+}
+
+print(open('./config/config.json', 'r').read())
 
 print('Create project on Jenkins', config['repo_name'])
 # req = Request('http://ci.anvil8.com/createItem?name={{', data, headers)
