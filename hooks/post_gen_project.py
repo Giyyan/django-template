9ebb963268a861401fa1b9c79c16f57c12d58ac1
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2014-10-20 17:25:04 +0300

    Test tag in hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import shutil
+import json
 try:
     # For Python 3.0 and later
     from urllib.request import Request
@@ -6,7 +7,9 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import Request
 
-print('Create project on Jenkins {{cookiecutter.repo_name}}')
+config = json.loads(open('./config/config.json'))
+
+print('Create project on Jenkins', config['repo_name'])
 # req = Request('http://ci.anvil8.com/createItem?name={{', data, headers)
 
 shutil.rmtree('./config')
