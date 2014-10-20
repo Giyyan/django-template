c567b78113e53f90e1124675f528cb97ad76fa89
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2014-10-20 17:19:29 +0300

    Test tag in hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,7 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import Request
 
-print('Create project on Jenkins {{cookicutter.repo_name}}')
+print('Create project on Jenkins {{cookiecutter.repo_name}}')
 # req = Request('http://ci.anvil8.com/createItem?name={{', data, headers)
 
 shutil.rmtree('./config')
