6ff5bd5a7ab366002f791c8bc579ef1254a170a6
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2014-10-21 14:19:56 +0300

    Add create project on Jenkins and init git repo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,7 @@ import shutil
 import json
 from jenkinsapi.jenkins import Jenkins
 from fabric.api import local
+import requests
 
 
 config = json.loads(open('./config/config.json', 'r').read())
@@ -11,7 +12,13 @@ J = Jenkins('http://ci.anvil8.com',
             password=config.get('ci_apitoken'))
 
 if not config.get('git_url'):
-    pass
+    data = {
+        'name': config.get('repo_name'),
+        'description': config.get('description'),
+    }
+    r = requests.post("http://git.anvil8.com/api/v3/projects?private_token={}".format(config.get('gitlab_token'),))
+    response = json.loads(r.json())
+    config['git_url'] = 'git@git.anvil8.com:{}.git'.format(response.get('path_with_namespace'),)
 
 local('git init')
 local('git remote add origin {}'.format(config.get('git_url')))
