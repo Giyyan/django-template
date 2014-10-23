8b13a738469996eed752f2a4651972ab36c39f06
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2014-10-23 14:46:05 +0300

    add jenkins user to repo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,12 +6,17 @@ import requests
 
 
 config = json.loads(open('./config/config.json', 'r').read())
-
+"""
+Connecting to Jenkins
+"""
 J = Jenkins('http://ci.anvil8.com',
             username=config.get('ci_username'),
             password=config.get('ci_apitoken'))
 
 if not config.get('git_url'):
+    """
+    Create repo on git.anvil8.com
+    """
     create_project_data = {
         'name': config.get('repo_name'),
         'description': config.get('description'),
@@ -22,14 +27,20 @@ if not config.get('git_url'):
     print("Create git repo on http://git.anvil8.com")
     config['git_url'] = 'git@git.anvil8.com:{}.git'.format(response.get('path_with_namespace'),)
 
+    """
+    Add jenkins user as reporter to repo
+    """
     add_member_data = {
         'id': response.get('id'),
         'user_id': 28,
         'access_level': 20
     }
-    r = requests.post("http://git.anvil8.com/api/v3/project/{}/members?private_token={}".format(config.get('gitlab_token'), response.get('id')), data=add_member_data)
+    r = requests.post("http://git.anvil8.com/api/v3/projects/{}/members?private_token={}".format(config.get('gitlab_token'), response.get('id')), data=add_member_data)
     print(r)
 
+"""
+Init git repo and push project
+"""
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'remote', 'add', 'origin', config.get('git_url')])
 subprocess.call(['git', 'add', '.'])
